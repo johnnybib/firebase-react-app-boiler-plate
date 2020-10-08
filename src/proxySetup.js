42a4de90ef1a6fc80348b2b634a9c4788279f29c
johnnybib <jamesgrahamhu@gmail.com>
2020-10-08 15:32:09 -0600

    Making some names generic

--- src/proxySetup.js
+++ src/proxySetup.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5001/entid-9c711/us-central1/server',
+      target: 'http://localhost:PORT',
       changeOrigin: false,
     })
   );
